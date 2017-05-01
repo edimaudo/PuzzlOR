180d4db62097108abb2bf3c5b9a7a3c8816764f7
Edima Udo <edimaudo@gmail.com>
2017-05-01 14:44:24 -0400

    Update

--- PuzzlORDecember2016.py
+++ PuzzlORDecember2016.py
@@ -1,4 +1,4 @@
-#PuzzlOR December 2016 - Galaxy on fire
+ 
 import sys
 import operator
 from operator import itemgetter
@@ -10,16 +10,16 @@ def main():
 		Ship Name, Cargo, Weapons, Equipment, Cost
 		"""
 		ship_info = {
-			"Cicero":[25,2,6,51975],
+			"Cicero":	[25,2,6,51975],
 			"Cormorant":[350,0,8,168900],
-			'Dace':[38,4,5,235600],
+			'Dace':		[38,4,5,235600],
 			'Hatsuyuki':[28,2,8,171900],
-			"H'Soc":[45,2,7,150000],
-			"N'Tirrk":[80,2,8,250000],
-			"Razor":[6,60,4,6,294900],
-			"Taipan":[50,3,5,100100],
-			"Teneta":[65,2,7,125400],
-			"Type":[43,30,2,6,72500]
+			"H'Soc":	[45,2,7,150000],
+			"N'Tirrk":	[80,2,8,250000],
+			"Razor6":	[60,4,6,294900],
+			"Taipan":	[50,3,5,100100],
+			"Teneta":	[65,2,7,125400],
+			"Type43":	[30,2,6,72500]
 		}
 
 		cargo_info = [[ship,ship_info[ship][0]] for ship in ship_info]
