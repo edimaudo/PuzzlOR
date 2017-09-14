10d43a9c9216a49c2af1e6d44a84cfb3af2a4ec2
Edima Udo <edimaudo@gmail.com>
2017-09-14 10:14:15 -0400

    PuzzlOR

--- PuzzlORFebruary2017.py
+++ PuzzlORFebruary2017.py
@@ -4,36 +4,14 @@
 
 #31
 import sys
-
+import itertools
 
 def main():
 	try:
-		dockSize =  [4,7,9]
-		clusterSize = [8,5,7]
+		clusterInfo = ["cluster1","cluster2","cluster3"]
+		for path in itertools.permutations(clusterInfo):
+			print(path) 
 
-		#cluster distance w.r.t docks
-		cluster1distance = [1,2,3]
-		cluster2distance = [2,1,2]
-		cluster3distance = [3,2,1]
-
-		distance  = 0
-		
-
-		#initial cluster assignment
-		clusterCheck  = [clusterSize[i] - dockSize[i] for i in range(len(clusterSize))]
-
-		positionData = []
-		count = 0
-		for check in clusterCheck:
-			if check < 0:
-				positionData.append(count)
-			count+=1
-		
-		distance = clusterSize[positionData[0]] + clusterSize[positionData[1]] + clusterSize[0]-4
-		+ 2*(cluster1distance[positionData[0]] + cluster1distance[positionData[1]])
-
-
-		print distance
 	except:
 		e = sys.exc_info()
 		print(e)
