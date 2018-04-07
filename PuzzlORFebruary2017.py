01978d2410d21939499e98a979232f9187624c4e
Edima Udo <edimaudo@gmail.com>
2018-04-07 17:18:46 -0400

    PuzzlORFebruary2017

--- PuzzlORFebruary2017.py
+++ PuzzlORFebruary2017.py
@@ -2,20 +2,20 @@
 #minimum possible total distance traveled by all boats
 
 
-#31
-import sys
-import itertools
+# #31
+# import sys
+# import itertools
 
-def main():
-	try:
-		clusterInfo = ["cluster1","cluster2","cluster3"]
-		for path in itertools.permutations(clusterInfo):
-			print(path) 
+# def main():
+# 	try:
+# 		clusterInfo = ["cluster1","cluster2","cluster3"]
+# 		for path in itertools.permutations(clusterInfo):
+# 			print(path) 
 
-	except:
-		e = sys.exc_info()
-		print(e)
-		sys.exit()
+# 	except:
+# 		e = sys.exc_info()
+# 		print(e)
+# 		sys.exit()
 
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
