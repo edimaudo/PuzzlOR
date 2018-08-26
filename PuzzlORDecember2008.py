b09446cc609d63aaa140eb388a16837b913b43ec
Edima Udo <edimaudo@gmail.com>
2018-08-26 15:52:53 -0400

    update

--- PuzzlORDecember2008.py
+++ PuzzlORDecember2008.py
@@ -1,3 +1,11 @@
-#5 x 5 poker
+# PuzzlOR 5 x 5 poker
 
 #What arrangement of cards maximizes the total point value from all 10 hands?
+#44 points is the maximum
+
+import math
+import sys
+
+
+if __name__ == "__main__":
+	main()
