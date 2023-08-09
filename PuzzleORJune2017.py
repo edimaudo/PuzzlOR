1861bad9f8ecdf71e0a5101e54268e11bf08fe4f
Edima <edimaudo@gmail.com>
2023-08-08 21:48:44 -0400

    Update PuzzleORJune2017.py

--- PuzzleORJune2017.py
+++ PuzzleORJune2017.py
@@ -1,8 +1,22 @@
 
+#PuzzlOR June 2017
+#https://pubsonline.informs.org/do/10.1287/orms.2017.03.22/full/
+
+
+import math
+import random
+from collections import Counter
+import operator
+
+def generate_random(alist):
+    return random.randint(0,len(alist)-1)
+
 def main():
     route = ""
     cost = 1000
     iterations = 10000
+    start_posiiton = 1
+    end_position = 31
     simulation_check = True
     
 
@@ -12,43 +26,44 @@ if __name__ = "__main__":
 
 data_position = 
 {
-1:[2],
-2:[3,4],
-3:
-4:
-5:
-6:
-7:
-8:
-9:
-10:
-11:
-12:
-13:
-14:
-15:
-16:
-17:
-18:
-19:
-20:
-21:
-22:
-23:
-24:
-25:
-26:
-27:
-28:
-29:
-30:
+    2:[3,4],
+    3:
+    4:
+    5:
+    6:
+    7:
+    8:
+    9:
+    10:
+    11:
+    12:
+    13:
+    14:
+    15:
+    16:
+    17:
+    18:
+    19:
+    20:
+    21:
+    22:
+    23:
+    24:
+    25:
+    26:
+    27:
+    28:
+    29:
+    30:
 
 }
 
-data_cost ={
+data_cost =
+{
+    1::0
     2:0
-    3:
-    4:
+    3:0
+    4:0
     5:
     6:
     7:
